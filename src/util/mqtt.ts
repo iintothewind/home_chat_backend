060990ff2e974865d84540ceb5ccef774be19d8f
iintothewind <ivarchen@live.cn>
2021-01-15 17:59:19 +0800

    added error handling to enqueue

--- src/util/mqtt.ts
+++ src/util/mqtt.ts
@@ -2,13 +2,10 @@ import { cfg } from '../util'
 import { enqueue } from '../util/redis'
 import { connect, MqttClient } from 'mqtt'
 
-console.log(`trying to connect: ${cfg.mqtt.url}`)
-
 const mqttClient: MqttClient = connect(cfg.mqtt.url, { clean: true, clientId: cfg.mqtt.sender, rejectUnauthorized: false })
 mqttClient
   .on('error', error => console.warn('mqtt error: '.concat(error.message)))
   .on('connect', () => {
-    console.log(`connected : ${cfg.mqtt.url}`)
     mqttClient.subscribe({ [cfg.mqtt.wildcardTopic]: { qos: 2 } }, (error, granted) => {
       if (error) {
         console.warn(`mqtt url: ${cfg.mqtt.url} subscription failed: ${error.message}`)
@@ -17,7 +14,11 @@ mqttClient
   })
   .on('message', (_topic, buffer) => {
     const payload = buffer.toString()
-    enqueue(_topic, payload)
+    enqueue(_topic, payload).catch(error=>{
+      if(error) {
+        console.error(`equeue error: ${error}`)
+      }
+    })
   })
 
 export { mqttClient }
