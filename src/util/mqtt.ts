4ba386cfe428abb5ff4ea98bf175ff45cbfffd1c
iintothewind <ivarchen@live.cn>
2021-01-24 12:53:38 +0800

    fixed http ssl issue

--- src/util/mqtt.ts
+++ src/util/mqtt.ts
@@ -5,20 +5,21 @@ import { enqueue } from '../util/redis'
 import { Message, Translation } from '../model'
 import { connect, MqttClient } from 'mqtt'
 
+const axiosInstance = axios.create({
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false
+  })
+})
+
 const consumeDict = (topic: string, message: Message) => {
   if (topic && message && message.content && message.content.startsWith(cfg.dict.operation)) {
     const user = message.sender
     const word = message.content.substr(cfg.dict.operation.length)
     const headers = { 'Accept': 'application/json' }
-    axios
-      .create({
-        httpsAgent: new https.Agent({
-          rejectUnauthorized: false
-        })
-      })
+    axiosInstance
       .get<Translation>(`https://${cfg.dict.host}:${cfg.dict.port}/home_dict/translate`, { params: new URLSearchParams({ user: user, word: word }), headers: headers })
       .then(resp => `单词: ${resp.data.word} \n读音: ${resp.data.phonetic} \n英义: ${resp.data.definition} \n翻译: ${resp.data.translation} \n变换: ${resp.data.exchange}`)
-      .then(translation => axios.post<Message>(`https://localhost:${cfg.https.port}/home_chat/message`, { topic: topic, content: translation } as Message))
+      .then(translation => axiosInstance.post<Message>(`https://localhost:${cfg.https.port}/home_chat/message`, { topic: topic, content: translation } as Message))
       .catch(error => console.log(`error in conumeDict: ${error}`))
   }
 }
